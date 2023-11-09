7280c05058e6c29af51c143823ce4c0aaf260f3c
FacuRodriJ <facurodri00@gmail.com>
2023-11-09 10:31:45 -0300

    admin.py actualizado

--- django_setup/core/admin.py
+++ django_setup/core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Municipio, Rendicion, Presentacion, DocumentoRequerido
+from .models import Municipio, Rendicion, Presentacion, DocumentoRequerido, Documento
 
 admin.site.register(Municipio)
 admin.site.register(Rendicion)
 admin.site.register(Presentacion)
 admin.site.register(DocumentoRequerido)
+admin.site.register(Documento)
