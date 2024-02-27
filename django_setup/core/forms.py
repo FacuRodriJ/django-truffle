7ebb8a5ca6fab4ab60f2a5f37d708e07b9279095
FacuRodriJ <facurodri00@gmail.com>
2024-02-27 00:33:54 -0300

    empty_label agregado.

--- django_setup/core/forms.py
+++ django_setup/core/forms.py
@@ -15,3 +15,7 @@ class DocumentoForm(forms.ModelForm):
             "archivo": forms.FileInput(attrs={"class": "form-control"}),
             "documento_requerido": forms.Select(attrs={"class": "form-select"}),
         }
+    
+    def __init__(self, *args, **kwargs):
+        super(DocumentoForm, self).__init__(*args, **kwargs)
+        self.fields['documento_requerido'].empty_label = "Seleccione una opción"
